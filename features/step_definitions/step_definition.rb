9155dae3e5f00d8b3d3f6067b40ddec6576fb240
warty <warty24@gmail.com>
2020-07-08 18:24:50 +0300

    deleted redirect

--- features/step_definitions/step_definition.rb
+++ features/step_definitions/step_definition.rb
@@ -6,17 +6,15 @@ require 'selenium/webdriver/firefox'
 require 'rubygems'
 require 'rspec'
 require_relative 'actions.rb'
-driver ||= " "
-element ||= " "
+driver ||= ""
+element ||= ""
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--disable-popup-blocking')
 options.add_argument('--disable-translate')
 
 Given(/^opened google search page$/) do
-  driver = Selenium::WebDriver.for :chrome, options: options
-  driver.get('https://4pda.ru/forum')
-  sleep(1)
+  driver = Selenium::WebDriver.for :firefox, options: options
   driver.get('https://www.bing.com')
 end
 
@@ -34,4 +32,7 @@ end
 
 Then(/^title should contain (.*)$/) do |searchStr|
   raise "Wrong condition!" unless driver.title.casecmp(searchStr)
+
+  sleep(1)
+  driver.close
 end
